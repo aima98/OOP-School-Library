2b1b919b0b2f483c6fb41fe4ba62f672d8bd417f
Oumaima Nejjari <oumanj1998@gmail.com>
2023-07-01 01:24:20 +0100

    Fixing linter errors

--- person.rb
+++ person.rb
@@ -9,11 +9,12 @@ class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1...100)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
@@ -22,6 +23,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
 
   private
